995e195658eecbec97d3a9f674c300555b8f1de6
Jamaleh <Jamaleh@hotmail.com>
2015-07-11 17:31:53 +1000

    validate wikis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end 
+
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class; 'form-group'
+    end 
+  end 
 end
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,6 +2,11 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
   has_many :collaborators, through: :user
 
+  default_scope { order('created_at DESC') }
+
+  validates :title, length: { minimum: 3 }, presence: true
+  validates :body, length: {minimum: 10 }, presence: true
+
   # def self.visible_to(user)
 
   #   if user.present? && (user.admin? || user.premium?)
