511d3cfd270ca3eb79922544343359c09b40596f
Jamaleh <Jamaleh@hotmail.com>
2015-07-13 11:14:10 +1000

    problems with policy

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -5,36 +5,38 @@ class WikiPolicy < ApplicationPolicy
     true
   end 
 
+  
 
-  # class Scope
-  #   attr_reader :user, :scope
 
-  #   def initialize(user, scope)
-  #     @user = user
-  #     @scope = scope
-  #   end 
+  class Scope
+    attr_reader :user, :scope
 
-  #   def resolve
-  #     wikis = []
-  #     if user.role == 'admin'
-  #       wikis = scope.all 
-  #     elsif user.role == 'premium'
-  #       all_wikis = scope.all
-  #       all_wikis.each do |wiki|
-  #         if wiki.private != true || wiki.user == user || wiki.users.include?(user)
-  #           wikis << wiki
-  #         end
-  #       end 
-  #     else 
-  #       all_wikis = scope.all 
-  #       wikis = []
-  #       all_wikis.each do |wiki|
-  #         if wiki.private != true || wiki.users.include?(user)
-  #           wikis << wiki
-  #         end 
-  #       end 
-  #     end 
-  #     wikis
-  #   end 
-  # end 
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end 
+
+    def resolve
+      wikis = []
+      if user.present? && user.role == 'admin'
+        wikis = scope.all 
+      elsif user.present? && user.role == 'premium'
+        all_wikis = scope.all
+        all_wikis.each do |wiki|
+          if wiki.private != true || wiki.user == user || wiki.users.include?(user)
+            wikis << wiki
+          end
+        end 
+      else 
+        all_wikis = scope.all 
+        wikis = []
+        all_wikis.each do |wiki|
+          if wiki.private != true || wiki.users.include?(user)
+            wikis << wiki
+          end 
+        end 
+      end 
+      wikis
+    end 
+  end 
 end
