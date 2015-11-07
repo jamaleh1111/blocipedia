9e179e3bbf5c91fb46f20d7b7bb42cd026d03b8a
Jamaleh <Jamaleh@hotmail.com>
2015-11-07 17:59:40 +1100

    Update edit registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,24 @@ class UsersController < ApplicationController
   end
 
   def update
-  end
+    if current_user.update_attributes(user_params)
+      flash[:notice] = "User information updated"
+      redirect_to edit_user_registration_path
+    else 
+      flash[:error] = "Invalid user information"
+      redirect_to edit_user_registration_path
+    end 
+  end 
 
   def show
     @user = User.find(params[:id])
     @wikis = policy_scope(Wiki).select {|w| w.user_id == @user.id}.paginate(page: params[:page], per_page: 5)
   end
 
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end 
   
 end
